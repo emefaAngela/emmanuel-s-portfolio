db3ef490baed997c5743470a71a64ff74d2fe968
emefaAngela <63966516+emefaAngela@users.noreply.github.com>
2022-06-28 16:56:10 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px',
+    },
+    extend: {
+      colors:{
+        lightwhite:'#FCFCFC',
+        deepblue:'#091E3A',
+        lightblue:'#2D9EE0',
+        ash:'#4F5268',
+        white:'#FFFFFF',
+        vectorblue:'rgba(39, 117, 202, 0.2)',
+        vectororange:'rgba(249, 170, 75, 0.28)',
+        lightviolet:'#F3F2FD'
+
+
+
+      }
+    },
+  },
+  plugins: [],
+}
